34524a8ddebe14d137991f385c380964fbe0fdf1
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-08-09 16:04:37 -0400

    Added docs

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject gears "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[marginalia "0.5.0"]])
