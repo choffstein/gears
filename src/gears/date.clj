ec4499739327347ff8d6a31a9dbf7029218862d8
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-09-05 15:59:15 -0400

    Fixed import errors

--- src/gears/date.clj
+++ src/gears/date.clj
@@ -7,20 +7,20 @@
 (defn todays-date
   "Today's date as a yyyy-MM-dd string, unless a format is provided."
   ([] (todays-date "yyyy-MM-dd"))
-  ([format] (let [df (new SimpleDateFormat format)
+  ([format] (let [df (new java.text.SimpleDateFormat format)
                   today (new java.util.Date)]
               (.format df today))))
 
 (defn current-time
   "The current time in yyyy-MM-dd'T'HH:mmZ unless a format is provided."
   ([] (current-time "yyyy-MM-dd'T'HH:mmZ"))
-  ([format]  (let [df (SimpleDateFormat. format)]
-               (.toString (.format df (Date.))))))
+  ([format]  (let [df (java.text.SimpleDateFormat. format)]
+               (.toString (.format df (java.util.Date.))))))
 
 (defn date-to-string
   "Convert a Java Date object to a yyyy-MM-dd formatted string, unless a format is provided."
   ([date] (date-to-string "yyyy-MM-dd"))
-  ([date format]  (let [df (SimpleDateFormat. format)]
+  ([date format]  (let [df (java.text.SimpleDateFormat. format)]
                     (.format df date))))
 
 (defn string-to-date
@@ -28,7 +28,8 @@
    automatically attempts to convert with yyyy-MM-dd and MM/dd/yyyy formats."
   [date-string & given-formats]
   (let [formats (or given-formats ["MM/dd/yyyy" "yyyy-MM-dd"])
-        date-objects (filter identity (map #(try (.parse (SimpleDateFormat. %) date-string)
+        date-objects (filter identity (map #(try (.parse (java.text.SimpleDateFormat. %)
+                                                         date-string)
                                                  (catch Exception e nil)) formats))]
     (if (empty? date-objects)
         (throw (java.text.ParseException. "Unable to parse string" 0))
