1f6f86ff53f47b6b3e2335be593d676459cfdadf
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-09-14 15:35:43 -0400

    Fixed bug in list / changed merge-maps-by-key to allow infinite number of maps

--- src/gears/hash_map.clj
+++ src/gears/hash_map.clj
@@ -13,16 +13,18 @@
 
 (defn merge-maps-by-key
   "Given two maps, merge shared keys into a list.
-   `(merge-sets-by-key {:a :b, :c :d} {:a :e, :c :f})`
+   `(merge-maps-by-key {:a :b, :c :d} {:a :e, :c :f})`
    -> `{:a (:b :e), :c (:d :f)}`"
-  [map-a map-b]
-  (let [all-key-values (set (concat (keys map-a) (keys map-b)))]
-    (list/map-list-to-hashmap all-key-values #(filter identity [(get map-a %) (get map-b %)]))))
+  [& maps]
+  (let [all-key-values (set (flatten (map keys maps)))]
+    (list/map-list-to-hashmap #(filter identity
+                                       (map (fn [m] (get m %)) maps))
+                              all-key-values)))
 
 (defn merge-maps-by-key-value
   "Given a list of 'sets' (using that term loosely here, because they aren't actual clojure sets,
    merge them by a given key.  For example:
-   `(merge-sets-by-key-value [[{:a 5 :b 6} {:a 6 :b 7} {:a 8 :b 9}] [{:a 5 :c 12} {:a 6 :c 14} {:a 8 :c 18}]] :a)`
+   `(merge-maps-by-key-value [[{:a 5 :b 6} {:a 6 :b 7} {:a 8 :b 9}] [{:a 5 :c 12} {:a 6 :c 14} {:a 8 :c 18}]] :a)`
    -> `[{:a 5 :b 6 :c 12} {:a 6 :b 7 :c 14} {:a 8 :b 9 :c 18}]`"
   [sets key]
   (let [all-key-values (set (flatten (map #(pmap (fn [m] (get m key)) %) sets)))]
--- src/gears/list.clj
+++ src/gears/list.clj
@@ -2,5 +2,5 @@
 
 (defn map-list-to-hashmap
   "Takes a function `f` and a list `l` and creates a hash-map with keys l and values `(f l)`"
-  [l f]
-  (reduce (fn [m e] (assoc m e (f e)))))
+  [f l]
+  (reduce (fn [m e] (assoc m e (f e))) {} l))
