2868a220f6c4092c49c09a52e50bdd63e4bd6adc
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-09-19 10:08:28 -0400

    Changed java deps to clj-time deps

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [mmemail "1.0.1"]
-                 [sandbar/sandbar "0.3.0"]]
+                 [sandbar/sandbar "0.3.0"]
+                 [clj-time "0.3.0"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
 		     [lein-marginalia "0.6.0"]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"])
--- src/gears/date_time.clj
+++ src/gears/date_time.clj
@@ -1,34 +1,29 @@
 (ns gears.date-time
-  (:import [java.util.Date]
-	   [java.text DateFormat SimpleDateFormat]
-	   [java.util.Calendar]
-           [java.text.ParseException]))
+  (:use [clj-time core coerce]))
 
 (defn todays-date-as-str
   "Today's date as a yyyy-MM-dd string, unless a format is provided."
   ([] (todays-date-as-str "yyyy-MM-dd"))
-  ([format] (let [df (new java.text.SimpleDateFormat format)
-                  today (new java.util.Date)]
-              (.format df today))))
+  ([format] (let [custom-formatter (formatter format))
+                  today (now)]
+              (unparse custom-formatter today))))
 
 (defn current-time-as-str
   "The current time in yyyy-MM-dd'T'HH:mmZ unless a format is provided."
-  ([] (current-time-as-str "yyyy-MM-dd'T'HH:mmZ"))
-  ([format]  (let [df (java.text.SimpleDateFormat. format)]
-               (.toString (.format df (java.util.Date.))))))
+  ([] (todays-date-as-str "yyyy-MM-dd'T'HH:mmZ")))
 
 (defn date-to-string
   "Convert a Java Date object to a yyyy-MM-dd formatted string, unless a format is provided."
   ([date] (date-to-string "yyyy-MM-dd"))
-  ([date format]  (let [df (java.text.SimpleDateFormat. format)]
-                    (.format df date))))
+  ([date format]  (let [custom-formater (formatter format)]
+                    (parse custom-formatter date))))
 
 (defn string-to-date
   "Convert a formatted string to a Java Date object.  If no format is given,
    automatically attempts to convert with yyyy-MM-dd and MM/dd/yyyy formats."
   [date-string & given-formats]
   (let [formats (or given-formats ["MM/dd/yyyy" "yyyy-MM-dd"])
-        date-objects (filter identity (map #(try (.parse (java.text.SimpleDateFormat. %)
+        date-objects (filter identity (map #(try (parse (custom-formatter %)
                                                          date-string)
                                                  (catch Exception e nil)) formats))]
     (if (empty? date-objects)
