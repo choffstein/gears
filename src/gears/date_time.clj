da400697b4d6e363d9b4bf9255e7a72b611aa6d5
Corey M. Hoffstein <corey@newfoundresearch.com>
2011-12-25 20:24:14 -0700

    Added type-hints to date-time

--- src/gears/date_time.clj
+++ src/gears/date_time.clj
@@ -6,25 +6,25 @@
 (defn todays-date []
   (time/now))
 
-(defn date-to-string
+(defn date-to-string ^String
   "Convert a Java Date object to a yyyy-MM-dd formatted string,
    unless a format is provided."
   ([date] (date-to-string date "yyyy-MM-dd"))
-  ([date format]  (let [custom-formatter (format/formatter format)]
+  ([date ^String format]  (let [custom-formatter (format/formatter format)]
                     (format/unparse custom-formatter date))))
-(defn todays-date-as-str
+(defn todays-date-as-str ^String
   "Today's date as a yyyy-MM-dd string, unless a format is provided."
   ([] (todays-date-as-str "yyyy-MM-dd"))
-  ([format] (date-to-string (todays-date) format)))
+  ([^String format] (date-to-string (todays-date) format)))
 
-(defn current-time-as-str
+(defn current-time-as-str ^String
   "The current time in yyyy-MM-dd'T'HH:mmZ unless a format is provided."
   ([] (todays-date-as-str "yyyy-MM-dd'T'HH:mmZ")))
 
 (defn string-to-date
   "Convert a formatted string to a Java Date object.  If no format is given,
    automatically attempts to convert with yyyy-MM-dd and MM/dd/yyyy formats."
-  [date-string & given-formats]
+  [^String date-string & given-formats]
   (let [formats (or given-formats ["MM/dd/yyyy" "yyyy-MM-dd"])
         date-objects (filter identity
                              (map #(try (format/parse (format/formatter %)
@@ -35,13 +35,13 @@
                                                date-string) 0))
         (first date-objects))))
 
-(defn date-to-long [date]
+(defn date-to-long ^long [date]
   (coerce/to-long date))
 
-(defn long-to-date [long]
+(defn long-to-date [^long long]
   (coerce/from-long long))
 
-(defn between?
+(defn between? ^boolean
   "Checks whether a date is within a given range of dates
 
   => (between? (clj-time.core/date-time 1987) (clj-time.core/date-time 1986)
