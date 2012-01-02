43c5951af6fca7c6d37da66f06b43c538a9827d1
Corey M. Hoffstein <corey@newfoundresearch.com>
2012-01-01 20:47:33 -0700

    updated failure and success macros

--- src/gears/macros.clj
+++ src/gears/macros.clj
@@ -1,16 +1,25 @@
-(def ^:dynamic *failure-pattern* nil)
+(defmacro while-failure
+  "A macro that keeps executing arguments until success is hit.  Standard failure
+   is identified as nil, but can be rebound.
+
+   => (while-failure nil? (identity nil) (identity nil) (identity 5) (identity 6) (identity nil))
+   5"
+  ([failure-fn x] x)
+  ([failure-fn x & more]
+     `(let [x# ~x]
+        (if (~failure-fn x#)
+          (while-failing ~@more)
+          x#))))
 
 (defmacro until-success
   "A macro that keeps executing arguments until success is hit.  Standard failure
    is identified as nil, but can be rebound.
 
-   => (until-success nil nil 5 6 nil)
-   5
-
-   => (binding [*failure-pattern* false] (until-success false (identity false) nil 6))
-   nil"
-  ([x] x)
-  ([x & more] `(let [x# ~x]
-                 (if (= *failure-pattern* x#)
-                   (until-success ~@more)
-                   x#))))
+   => (until-success #(not (nil? %)) (identity nil) (identity nil) (identity 5) (identity 6) (identity nil))
+   5"
+  ([success-fn x] x)
+  ([success-fn x & more]
+     `(let [x# ~x]
+        (if (~success-fn x#)
+          x#
+          (until-success ~@more)))))
